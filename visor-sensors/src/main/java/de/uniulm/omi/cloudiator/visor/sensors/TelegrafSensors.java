4c1789b55306616d50c4025ae98af437dfe53531
Muhammad Khattab <msskzx@gmail.com>
2018-09-05 21:22:17 +0200

    writing telegraf.conf
    given the required telegraf plugins by the user, the telegraf configuration file is written accordingly

---
+++ visor-sensors/src/main/java/de/uniulm/omi/cloudiator/visor/sensors/TelegrafSensors.java
@@ -0,0 +1,48 @@
+package de.uniulm.omi.cloudiator.visor.sensors;
+
+import de.uniulm.omi.cloudiator.visor.exceptions.SensorInitializationException;
+import de.uniulm.omi.cloudiator.visor.monitoring.AbstractSensor;
+import de.uniulm.omi.cloudiator.visor.monitoring.MonitorContext;
+import de.uniulm.omi.cloudiator.visor.monitoring.SensorConfiguration;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.lang.management.ManagementFactory;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+
+public class TelegrafSensors extends AbstractSensor {
+
+  @Override
+  protected void initialize(MonitorContext monitorContext,
+      SensorConfiguration sensorConfiguration) throws SensorInitializationException {
+    super.initialize(monitorContext, sensorConfiguration);
+    
+    /*
+	 * <String, String>
+	 * 
+	 * <"cpu", "totalcpu = true,percpu = true">
+	 */
+    
+    StringBuilder sb = new StringBuilder("");
+    String betweenParametersSeparator = ",";		
+    String fileName = "telegraf.conf";
+	BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName));
+			
+	for (Entry<String, String> entry : sensorConfiguration.entrySet()) 
+	{
+		sb.append("[[inputs." + entry.getKey() + "]]\n");
+		String[] parameters = entry.getValue().split(betweenParametersSeparator);
+		for(int i = 0;i<parameters.length;i++){
+			sb.append("  " + parameters[i]+"\n");
+		}
+		
+	}
+		
+	bufferedWriter.write(sb.toString());
+	bufferedWriter.close();
+  }
+  
+}
