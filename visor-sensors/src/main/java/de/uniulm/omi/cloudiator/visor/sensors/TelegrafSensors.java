bff7ba2d32659ee1e6042a32d9ce8d676c69eeec
Muhammad Khattab <msskzx@gmail.com>
2018-09-06 17:22:38 +0200

    adding plugins to the config
    writeConfig should add plugins to the config file (inputs or outputs). The method reads the config file, because it may have plugins running, and then adds the required plugins to it.

--- visor-sensors/src/main/java/de/uniulm/omi/cloudiator/visor/sensors/TelegrafSensors.java
+++ visor-sensors/src/main/java/de/uniulm/omi/cloudiator/visor/sensors/TelegrafSensors.java
@@ -5,49 +5,60 @@ import de.uniulm.omi.cloudiator.visor.monitoring.AbstractSensor;
 import de.uniulm.omi.cloudiator.visor.monitoring.MonitorContext;
 import de.uniulm.omi.cloudiator.visor.monitoring.SensorConfiguration;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.management.ManagementFactory;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
-
 public class TelegrafSensors extends AbstractSensor {
 
-  @Override
-  protected void initialize(MonitorContext monitorContext,
-      SensorConfiguration sensorConfiguration) throws SensorInitializationException {
-    super.initialize(monitorContext, sensorConfiguration);
-    
-    /*
-	 * <String, String>
-	 * 
-	 * <"cpu", "totalcpu = true,percpu = true">
-	 */
-    
-    StringBuilder sb = new StringBuilder("");
-    String betweenParametersSeparator = ",";		
-    String fileName = "telegraf.conf";
-	BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName));
-			
-	for (Entry<String, String> entry : sensorConfiguration.entrySet()) 
-	{
-		sb.append("[[inputs." + entry.getKey() + "]]\n");
-		String[] parameters = entry.getValue().split(betweenParametersSeparator);
-		for(int i = 0;i<parameters.length;i++){
-			sb.append("  " + parameters[i]+"\n");
-		}
-		
+	@Override
+	protected void initialize(MonitorContext monitorContext, SensorConfiguration sensorConfiguration)
+			throws SensorInitializationException {
+		super.initialize(monitorContext, sensorConfiguration);
 	}
-		
-	bufferedWriter.write(sb.toString());
-	bufferedWriter.close();
-  }
-  
-  void run() throws IOException {
+
+	/*
+	 * read the config file and add the required inputs or outputs plugins
+	 * 
+	 * sensorType: "inputs" / "outputs"
+	 * 
+	 * sensorConfiguration: <"cpu", "totalcpu = true,percpu = true">
+	 */
+	void writeConfig(String sensorType, Map<String, String> sensorConfiguration) throws IOException {
+		String fileName = "telegraf.conf";
+		String betweenParametersSeparator = ",";
+
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+		StringBuilder stringBuilder = new StringBuilder("");
+
+		String st = "";
+		while ((st = bufferedReader.readLine()) != null) {
+			stringBuilder.append(st + "\n");
+		}
+		bufferedReader.close();
+
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName));
+
+		for (Entry<String, String> entry : sensorConfiguration.entrySet()) {
+			stringBuilder.append("[[" + sensorType + ":" + entry.getKey() + "]]\n");
+			String[] parameters = entry.getValue().split(betweenParametersSeparator);
+			for (int i = 0; i < parameters.length; i++) {
+				stringBuilder.append("  " + parameters[i] + "\n");
+			}
+
+		}
+
+		bufferedWriter.write(stringBuilder.toString());
+		bufferedWriter.close();
+	}
+
+	void run() throws IOException {
 		Runtime.getRuntime().exec("telegraf.exe");
 	}
-  
+
 }
