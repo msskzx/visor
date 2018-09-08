8c39b2fc3f6d2d53b220c492ee8b064009791100
Muhammad Khattab <msskzx@gmail.com>
2018-09-08 07:12:35 +0200

    create config file
     - create config file with global settings
     - draft for the output plugin responsible for sending metrics

--- visor-sensors/src/main/java/de/uniulm/omi/cloudiator/visor/sensors/TelegrafSensors.java
+++ visor-sensors/src/main/java/de/uniulm/omi/cloudiator/visor/sensors/TelegrafSensors.java
@@ -16,6 +16,9 @@ import java.util.Map.Entry;
 
 public class TelegrafSensors extends AbstractSensor {
 
+	String betweenParametersSeparator = ",";
+	String fileName = "Telegraf/telegraf.conf";
+
 	@Override
 	protected void initialize(MonitorContext monitorContext, SensorConfiguration sensorConfiguration)
 			throws SensorInitializationException {
@@ -29,9 +32,7 @@ public class TelegrafSensors extends AbstractSensor {
 	 * 
 	 * sensorConfiguration: <"cpu", "totalcpu = true,percpu = true">
 	 */
-	void writeConfig(String sensorType, Map<String, String> sensorConfiguration) throws IOException {
-		String fileName = "telegraf.conf";
-		String betweenParametersSeparator = ",";
+	void addPlugin(String sensorType, Map<String, String> sensorConfiguration) throws IOException {
 
 		BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
 		StringBuilder stringBuilder = new StringBuilder("");
@@ -53,12 +54,48 @@ public class TelegrafSensors extends AbstractSensor {
 
 		}
 
+		stringBuilder.append("\n");
 		bufferedWriter.write(stringBuilder.toString());
+
 		bufferedWriter.close();
 	}
 
 	void run() throws IOException {
 		Runtime.getRuntime().exec("telegraf.exe");
+	}
+
+	void addHTTPPlugin() throws IOException {
+		String parameters = "url = \"http://localhost/monitors\"\n";
+		Map<String, String> sensorConfiguration = new HashMap<>();
+		sensorConfiguration.put("http", parameters);
+
+		addPlugin("outputs", sensorConfiguration);
+	}
+
+	void setConfigSettings(String global_tags, String agent) throws IOException {
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName));
+		StringBuilder stringBuilder = new StringBuilder("");
+
+		stringBuilder.append("[global_tags]\n");
+		String[] global_tagsParameters = global_tags.split(betweenParametersSeparator);
+		for (int i = 0; i < global_tagsParameters.length; i++)
+			stringBuilder.append("  " + global_tagsParameters[i] + "\n");
+		stringBuilder.append("\n");
+
+		stringBuilder.append("[agent]\n");
+		String[] agentParameters = agent.split(betweenParametersSeparator);
+		for (int i = 0; i < agentParameters.length; i++)
+			stringBuilder.append("  " + agentParameters[i] + "\n");
+		stringBuilder.append("\n");
+
+		bufferedWriter.write(stringBuilder.toString());
+		bufferedWriter.close();
+	}
+
+	void createConfig() throws IOException {
+		String global_tags = "";
+		String agent = "interval = \"10s\",round_interval = true,flush_buffer_when_full = true,collection_jitter = \"0s\",flush_interval = \"10s\",flush_jitter = \"0s\",debug = false,quiet = false,logfile = \"Telegraf/telegraf.log\",hostname = \"\"";
+		setConfigSettings(global_tags, agent);
 	}
 
 }
